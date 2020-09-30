dbb6cbe032eaf1c36d671051d182388fa7ba391a
Julian Hofer <julianhofer@gnome.org>
2020-09-30 19:30:27 +0200

    Use the builder-pattern for various widgets

--- src/widgets/pages/welcome.rs
+++ src/widgets/pages/welcome.rs
@@ -60,13 +60,15 @@ impl WelcomePageWidget {
     }
 
     fn init(&self) {
-        let container = gtk::Box::new(gtk::Orientation::Vertical, 0);
-
-        container.set_property_expand(true);
-        container.set_valign(gtk::Align::Center);
-        container.set_halign(gtk::Align::Center);
-        container.set_margin_top(24);
-        container.set_margin_bottom(24);
+        let container = gtk::BoxBuilder::new()
+            .orientation(gtk::Orientation::Vertical)
+            .spacing(0)
+            .expand(true)
+            .valign(gtk::Align::Center)
+            .halign(gtk::Align::Center)
+            .margin_top(24)
+            .margin_bottom(24)
+            .build();
 
         #[cfg(not(feature = "video"))]
         let header = {
@@ -154,23 +156,30 @@ impl WelcomePageWidget {
         text.show();
         container.add(&text);
 
-        let actions_container = gtk::Box::new(gtk::Orientation::Horizontal, 12);
-        actions_container.set_halign(gtk::Align::Center);
-        actions_container.set_margin_top(36);
+        let actions_container = gtk::BoxBuilder::new()
+            .orientation(gtk::Orientation::Horizontal)
+            .spacing(12)
+            .halign(gtk::Align::Center)
+            .margin_top(36)
+            .build();
 
-        let skip_tour_btn = gtk::Button::with_label(&gettext("_No Thanks"));
-        skip_tour_btn.set_property_height_request(40);
-        skip_tour_btn.set_property_width_request(180);
-        skip_tour_btn.set_use_underline(true);
-        skip_tour_btn.set_action_name(Some("app.skip-tour"));
+        let skip_tour_btn = gtk::ButtonBuilder::new()
+            .label(&gettext("_No Thanks"))
+            .height_request(40)
+            .width_request(180)
+            .use_underline(true)
+            .action_name("app.skip-tour")
+            .build();
         skip_tour_btn.show();
         actions_container.add(&skip_tour_btn);
 
-        let start_tour_btn = gtk::Button::with_label(&gettext("_Start Tour"));
-        start_tour_btn.set_property_height_request(40);
-        start_tour_btn.set_property_width_request(180);
-        start_tour_btn.set_use_underline(true);
-        start_tour_btn.set_action_name(Some("app.start-tour"));
+        let start_tour_btn = gtk::ButtonBuilder::new()
+            .label(&gettext("_Start Tour"))
+            .height_request(40)
+            .width_request(180)
+            .use_underline(true)
+            .action_name("app.start-tour")
+            .build();
         start_tour_btn.get_style_context().add_class("suggested-action");
         start_tour_btn.show();
         actions_container.add(&start_tour_btn);
